49223c87f6677582ad8d0a30dad1bed725edd1be
Fearhurst <cfairhurst@gmail.com>
2025-03-16 19:45:00 -0400

    fixed the header

--- app/view/include/top.php
+++ app/view/include/top.php
@@ -29,25 +29,22 @@
 
 	<body>
 		
-		<header class="is-center">
-			<div class="row">
-				<div class="col is-paddingless is-marginless">
-					
-<!-- 					<div class="hamburger-menu"> -->
-						<input id="menu__toggle" type="checkbox" />
-						<label class="menu__btn" for="menu__toggle">
-							<span></span>
-						</label>
-						<ul class="menu__box">
-							<li><a class="menu__item" href="schedule">Schedule</a></li>
-							<li><a class="menu__item" href="roster">Roster</a></li>
-							<li><a class="menu__item" href="#">Beer</a></li>
-							<li><hr /></li>
-							<li><a class="menu__item" href="logout">Logout</a></li>
-						</ul>
-<!-- 					</div> -->
-					  
-					<span class="header-title">Catfish Hockey</span>
-				</div>
+		<header class="text-justify" style="text-align: center;">
+
+			<div id="hamburger">
+				<input id="menu__toggle" type="checkbox" />
+				<label class="menu__btn" for="menu__toggle">
+					<span></span>
+				</label>
+				<ul class="menu__box">
+					<li><a class="menu__item" href="schedule">Schedule</a></li>
+					<li><a class="menu__item" href="roster">Roster</a></li>
+					<li><a class="menu__item" href="#">Beer</a></li>
+					<li><hr /></li>
+					<li><a class="menu__item" href="logout">Logout</a></li>
+				</ul>
 			</div>
+			  
+			<span class="header-title">Catfish Hockey</span>
+
 		</header>
